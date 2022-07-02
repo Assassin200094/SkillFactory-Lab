4bc3e0ae55240d3497fd3affe9c6f1b1bb16d6fc
Assassin20094 <thepunisher20199@gmail.com>
2022-07-02 16:28:17 +0300

    D_19

---
+++ project/app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import IndexView, CreateView, DetailView#, editor
+
+urlpatterns = [
+    path('', IndexView.as_view() ),
+    path('<int:cat>', IndexView.as_view() ),
+    path('create/<int:cat>', CreateView.as_view() ),
+    # path('create/', CreateView.as_view() ),
+    path('<int:cat>/post/<int:post>', DetailView.as_view()),
+    path('<int:cat>/post/<int:post>/clicks/', include('clicks.urls')),
+]
